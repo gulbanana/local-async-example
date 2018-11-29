c8b1cf240586a05473d9841dba963ce3243a7565
Thomas Castiglione <pd75840@police.wa.gov.au>
2018-11-29 16:48:27 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Handle(ThrowBeforeAwait);
+        Handle(ThrowAfterAwait);
+        Handle(AwaitThenDoWork);
+    }
+
+    static void Handle(Func<Task> f)
+    {
+        async Task localHandler()
+        {
+            try
+            {
+                await f();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Caught!");
+            }
+        }
+        localHandler().GetAwaiter().GetResult();
+    }
+
+    #pragma warning disable CS0162 // unreachable code 
+    static async Task ThrowBeforeAwait()
+    {
+        throw new Exception();
+        await Task.Yield();
+    }
+
+    static async Task ThrowAfterAwait()
+    {
+        await Task.Yield();
+        throw new Exception();
+    }
+
+    static async Task AwaitThenDoWork()
+    {
+        Console.WriteLine("On threadpool: " + Thread.CurrentThread.IsThreadPoolThread);
+        await Task.Yield();
+        Console.WriteLine("On threadpool: " + Thread.CurrentThread.IsThreadPoolThread);
+    }
+}
